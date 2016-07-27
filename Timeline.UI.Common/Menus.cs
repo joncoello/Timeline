a57a0f93ea2c10cd0ccd7d708b8986262cd44c33
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-07-27 19:31:08 +0100

    add maintenance menu for timelines

---
+++ Timeline.UI.Common/Menus.cs
@@ -0,0 +1,30 @@
+﻿using MYOB.CSSInterface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Timeline.UI.Common {
+    public class Menus : CSSMenus {
+
+        public Menus() {
+            ProductName = "Timeline";
+
+            var mnuMaintenance = Add("mnuMaintenance", "Maintenance", MenuItemType.Group);
+            var mnuTimeline = mnuMaintenance.Add("mnuTimeline", "Timeline", CSSMenuItem.MenuItemType.Group);
+            mnuTimeline.Add("mnuTimelines", "Timelines", CSSMenuItem.MenuItemType.Item);
+        }
+
+        public override void PluginMenuClicked(object Sender, MenuEventArgs e) {
+            
+            switch (e.Key) {
+                case "mnuTimelines":
+                    MessageBox.Show("Hello");
+                    break;
+            }
+            
+        }
+    }
+}
