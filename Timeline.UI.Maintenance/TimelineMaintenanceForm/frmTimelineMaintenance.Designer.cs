dd9f01dd03438be6011482386f1107c82371940f
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-07-27 20:27:42 +0100

    add in memory timeline definition repo

--- Timeline.UI.Maintenance/TimelineMaintenanceForm/frmTimelineMaintenance.Designer.cs
+++ Timeline.UI.Maintenance/TimelineMaintenanceForm/frmTimelineMaintenance.Designer.cs
@@ -24,16 +24,33 @@
         /// </summary>
         private void InitializeComponent() {
             this.grdTimelines = new Janus.Windows.GridEX.GridEX();
+            this.lblInfo = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.grdTimelines)).BeginInit();
             this.SuspendLayout();
             // 
             // grdTimelines
             // 
-            this.grdTimelines.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.grdTimelines.Location = new System.Drawing.Point(0, 0);
+            this.grdTimelines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grdTimelines.Location = new System.Drawing.Point(12, 125);
             this.grdTimelines.Name = "grdTimelines";
-            this.grdTimelines.Size = new System.Drawing.Size(636, 390);
+            this.grdTimelines.Size = new System.Drawing.Size(612, 253);
             this.grdTimelines.TabIndex = 0;
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInfo.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.lblInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblInfo.Location = new System.Drawing.Point(12, 9);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(612, 99);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Manage your timelines from here.\r\n\r\nTo create a new time simply use the blank row" +
+    " at the top, enter a name then click enter.\r\n\r\nTo edit an existing timeline, dou" +
+    "ble click on the row.\r\n";
             // 
             // frmTimelineMaintenance
             // 
@@ -41,6 +58,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(636, 390);
+            this.Controls.Add(this.lblInfo);
             this.Controls.Add(this.grdTimelines);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmTimelineMaintenance";
@@ -53,5 +71,6 @@
         #endregion
 
         private Janus.Windows.GridEX.GridEX grdTimelines;
+        private System.Windows.Forms.Label lblInfo;
     }
 }
--- Timeline.UI.Maintenance/TimelineMaintenanceForm/frmTimelineMaintenance.cs
+++ Timeline.UI.Maintenance/TimelineMaintenanceForm/frmTimelineMaintenance.cs
@@ -1,4 +1,5 @@
-﻿using MYOB.CSSInterface;
+﻿using Janus.Windows.GridEX;
+using MYOB.CSSInterface;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timeline.DomainModel.Models;
 using Timeline.DomainModel.Repositories;
 using Timeline.UI.Common.FormsFactory;
 
@@ -36,19 +38,41 @@ namespace Timeline.UI.Maintenance.TimelineMaintenanceForm {
         protected override void OnLoad(EventArgs e) {
             base.OnLoad(e);
 
+            FormatScreen();
             InitGrid();
             LoadData();
         }
 
+        private void FormatScreen() {
+            this.Text = "Timelines";
+        }
+
         private void InitGrid() {
+
+            GridEXColumn col;
+            var rt = new GridEXTable();
+
+            rt.RowHeight = 30;
+
+            grdTimelines.RootTable = rt;
+            grdTimelines.ColumnAutoResize = true;
+            grdTimelines.VisualStyle = VisualStyle.Office2010;
+            grdTimelines.AlternatingColors = true;
+            grdTimelines.GroupByBoxVisible = false;
+            
+            col = rt.Columns.Add("Name");
+            col.DataMember = "TimelineDefinitionName";
+            col.EditType = EditType.NoEdit;
+            col.Selectable = false;
             
         }
 
         private void LoadData() {
+
             var repo = new TimelineDefinitionRepository();
             var definitions = repo.Get();
             grdTimelines.SetDataBinding(definitions, string.Empty);
-            grdTimelines.RetrieveStructure();
+            
         }
     }
 }
