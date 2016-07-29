0a8b1b11399321434b69a3521f1c1cab83355c24
Jon.Coello <Jon.Coello@WLKCND4071HPV>
2016-07-29 13:29:40 +0100

    in memory example of binding

--- Timeline.UI.Homepage/ctlTimelines.cs
+++ Timeline.UI.Homepage/ctlTimelines.cs
@@ -8,12 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Threading;
 
 namespace Timeline.UI.Homepage {
     public partial class ctlTimelines : HomepageBase {
+        private DataTable data;
 
         public ctlTimelines() {
             InitializeComponent();
+
+            FormatChart();
+        }
+
+        private void FormatChart() {
+            chtTimeline.Series[0].XValueMember = "Milestone";
+            chtTimeline.Series[0].YValueMembers = "Count";
         }
 
         public override string DisplayName {
@@ -23,11 +32,47 @@ namespace Timeline.UI.Homepage {
         }
 
         public override void DefaultSettings() {
-            
+
         }
 
         public override void LoadData(bool Cache, bool StartNewThread) {
+            if (!Cache) {
+                if (StartNewThread) {
+                    var t = new Thread(GetData);
+                    t.Start();
+                } else {
+                    GetData();
+                }
+            }
+        }
+
+        private void GetData() {
+
+            Thread.Sleep(2000);
+
+            // get data
+            data = new DataTable();
+            data.Columns.Add("Milestone", typeof(string));
+            data.Columns.Add("Count", typeof(int));
+
+            data.Rows.Add("Information requested", 432);
+            data.Rows.Add("Information received", 132);
+            data.Rows.Add("Processing return", 86);
+            data.Rows.Add("In review", 91);
+            data.Rows.Add("Client Approval", 42);
+            data.Rows.Add("Submitted", 23);
+
+            // refresh control
+            if (this.InvokeRequired && this.IsHandleCreated) {
+                this.Invoke(new Action(BindData));
+            }
+        }
+
+        private void BindData() {
             
+            chtTimeline.DataSource = data;
+            chtTimeline.DataBind();
+
         }
 
         public override void RestoreCustomisation(XmlElement Customisation) {
